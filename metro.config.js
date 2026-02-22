5b15758b05f0893dff01138ed157875c86c462dd
tunenj <yakubyusuf6@gmail.com>
2026-02-22 23:40:48 +0100

    add stripe web resolver to metro config

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,16 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require('nativewind/metro');
- 
-const config = getDefaultConfig(__dirname)
- 
-module.exports = withNativeWind(config, { input: './global.css' })
+
+const config = getDefaultConfig(__dirname);
+
+config.resolver.resolveRequest = (context, moduleName, platform) => {
+  if (platform === 'web' && moduleName === '@stripe/stripe-react-native') {
+    return {
+      filePath: require.resolve('./mocks/stripe-react-native.web.js'),
+      type: 'sourceFile',
+    };
+  }
+  return context.resolveRequest(context, moduleName, platform);
+};
+
+module.exports = withNativeWind(config, { input: './global.css' });
